8db358e9914b16be222b36feb7f2805009d2c383
Jacob Bieker <jacob@bieker.tech>
2016-02-07 15:29:19 -0800

    Copying files

--- move-pdf.py
+++ move-pdf.py
@@ -1,1 +1,11 @@
 __author__ = 'jacob'
+import os
+from shutil import copyfile
+
+common_path = os.path.join("")
+initial_path = os.path.join("", common_path)
+
+final_path = os.path.join("", common_path)
+
+for file_name in initial_path:
+    copyfile(os.path.join(initial_path, file_name), os.path.join(final_path, file_name))
