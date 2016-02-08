05e9669d17814f2e222a3454d6c5a8a48cd150c8
Jacob Bieker <jacob@bieker.tech>
2016-02-08 10:48:26 -0800

    Fix paths

--- move-pdf.py
+++ move-pdf.py
@@ -2,7 +2,7 @@ __author__ = 'jacob'
 import os
 from shutil import copyfile
 
-common_path = os.path.join("RC_Data_FMS", "AppsSPUR", "22FebSPURapps07a", "PDF")
+common_path = os.path.join("RC_Data_FMS", "AppsSPUR", "Files", "AppsSPUR", "22FebSPURapps07a", "PDF")
 initial_path = os.path.join("Library", "FileMaker Server", "Data", "Databases", "SPUR", common_path)
 
 final_path = os.path.join("Server HD", "Users", "spur", "Documents", "Web", common_path)
