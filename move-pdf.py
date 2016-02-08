5849454aa4204f4076b0931ce711cd22d3940377
Jacob Bieker <jacob@bieker.tech>
2016-02-08 10:40:34 -0800

    Update paths

--- move-pdf.py
+++ move-pdf.py
@@ -2,10 +2,10 @@ __author__ = 'jacob'
 import os
 from shutil import copyfile
 
-common_path = os.path.join("")
-initial_path = os.path.join("", common_path)
+common_path = os.path.join("RC_Data_FMS", "AppsSPUR", "22FebSPURapps07a", "PDF")
+initial_path = os.path.join("Library", "FileMaker Server", "Data", "Databases", "SPUR", common_path)
 
-final_path = os.path.join("", common_path)
+final_path = os.path.join("Server HD", "Users", "spur", "Documents", "Web", common_path)
 
 for file_name in initial_path:
     copyfile(os.path.join(initial_path, file_name), os.path.join(final_path, file_name))
